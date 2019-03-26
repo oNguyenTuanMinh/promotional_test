9b75fc0768f64ee853d4703a8b42b61b3e50e168
oNguyenTuanMinh <nguyen.tuan.minh@framgia.com>
2019-03-26 19:10:17 +0700

    add unit test for test case #1

--- spec/logivan_test_spec.rb
+++ spec/logivan_test_spec.rb
@@ -3,13 +3,31 @@ require "./models/checkout.rb"
 require "./models/promotional_rule.rb"
 
 describe "logivan_test" do
-  let(:percentage_discount) do
-    PromotialRule.new.percentage_discount({ minimum: 60.00, percent: 10.00 })
-  end
+  describe "when discounts triggered" do
+    let(:percentage_discount) do
+      PromotialRule.new.percentage_discount({ minimum: 60.00, percent: 10.00 })
+    end
 
-  it "should discount 10% if total price is over 60" do
-    checkout = Checkout.new([percentage_discount])
-    checkout.scan(Product.new("001", "Lavender heart", 100.00))
-    checkout.total.equal?(90.00)
+    it "should discount 10% if total price is over 60" do
+      checkout = Checkout.new([percentage_discount])
+      checkout.scan(Product.new("001", "Lavender heart", 100.00))
+      expect(checkout.total).to equal(90.0)
+    end
+
+    test_case1 = <<-STR
+    -------------------------------------
+    Test case #1
+    Basket: 001,002,003
+    Total price expected: £66.78
+    -------------------------------------
+    STR
+
+    it test_case1 do
+      checkout = Checkout.new([percentage_discount])
+      checkout.scan(Product.new("001", "Lavender heart", 9.25))
+      checkout.scan(Product.new("002", "Personalised cufflinks", 45.00))
+      checkout.scan(Product.new("003", "are neat", 19.95))
+      expect(checkout.total).to equal(66.78)
+    end
   end
 end
